4b9e581498bb7cb048436732bc700499d60e427d
Georgii Semenov <themrgeorgeous@gmail.com>
2022-02-13 03:40:09 +0300

    heroku config

--- app.py
+++ app.py
@@ -55,18 +55,12 @@ def index():
 
 
 import torchvision
-from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
 model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
 
 import cv2
-import torch
+#import torch
 import numpy as np
-
-import matplotlib.pyplot as plt
-# %matplotlib inline
-
-import warnings
 
 def plot_preds(numpy_img, preds, color=(255, 0, 0)):
 
